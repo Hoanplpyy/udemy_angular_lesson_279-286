a6717a3020b60f1249a3e1356fab98ea31f0612b
hsien <hoanplpyy@hotmail.com>
2022-11-10 23:15:38 +0800

    283: 從firebase取recipes到程式中

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
@@ -8,20 +9,29 @@ import { RecipeService } from "../recipes/recipe.service";
 
 export class DataStorageService {
 
-  urlFireBase='https://httpproject-279-286-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  urlFireBase = 'https://httpproject-279-286-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
   constructor(private http: HttpClient, private recipeService: RecipeService) {
   }
 
   storeRecipes() {
-    const recipeUrl=this.urlFireBase+'recipes.json'
-    const recipes=this.recipeService.getRecipes();
-    this.http.put(recipeUrl,recipes)
-    .subscribe(
-      response=>{
-        console.log(response)
-      }
-    )
+    const recipeUrl = this.urlFireBase + 'recipes.json'
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(recipeUrl, recipes)
+      .subscribe(
+        response => {
+          console.log(response)
+        }
+      )
+  }
+
+  fetchRecipes() {
+    const recipeUrl = this.urlFireBase + 'recipes.json';
+    this.http
+      .get<Recipe[]>(recipeUrl)
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
   }
 
 }
