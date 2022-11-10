572a673fbfd6199d9eb295191b74eec1f830899b
hsien <hoanplpyy@hotmail.com>
2022-11-10 22:52:44 +0800

    282: 儲存recipes到firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DataStorageService {
-  constructor(private http: HttpClient) {
 
+  urlFireBase='https://httpproject-279-286-default-rtdb.asia-southeast1.firebasedatabase.app/';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes() {
+    const recipeUrl=this.urlFireBase+'recipes.json'
+    const recipes=this.recipeService.getRecipes();
+    this.http.put(recipeUrl,recipes)
+    .subscribe(
+      response=>{
+        console.log(response)
+      }
+    )
   }
 
 }
