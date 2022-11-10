751c2134a4aef14c051aa09afa3a6fd9136fbe5f
hsien <hoanplpyy@hotmail.com>
2022-11-10 23:38:21 +0800

    284: 將資料庫完全放到firebase，並且修正資料undefined的錯誤。

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 
@@ -29,6 +30,11 @@ export class DataStorageService {
     const recipeUrl = this.urlFireBase + 'recipes.json';
     this.http
       .get<Recipe[]>(recipeUrl)
+      .pipe(map(recipes=>{
+        return recipes.map(recipe=>{
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients :[]}
+        })
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       })
